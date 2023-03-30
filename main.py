4c2fbe3dd9a67b7f9c96344ffbd9b413556ab394
Bogdan264 <bogdanmychkovskyi@gmail.com>
2023-03-30 11:18:39 +0300

    Modify text.txt add additional check and modified main

--- main.py
+++ main.py
@@ -10,13 +10,24 @@ def removal_by_word_file (filename, keyword):
                 if keyword in line:
                     # якщо так, записуємо рядок у вихідний файл
                     f_out.write(line)
-        print("Filtered file saved as filtered.txt")
+        if os.path.exists(filename):
+            print("Filtered file saved as filtered.txt")
+        else:
+            print("No matches in file")
     except:
-        print(f"File {filename}.txt do not exist here")
+        print(f"File {filename} do not exist here")
 
 
 if __name__ == "__main__":
     filename = "text.txt"
     keyword = "damage"
+
+    #filename = input("Enter file name: ")
+    #keyword = input("Enter keyword: ")
+
+    format_file = ".txt"
+
+    if not format_file in filename:
+        filename += format_file
 
     removal_by_word_file(filename, keyword)
