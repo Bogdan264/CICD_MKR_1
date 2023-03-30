fede1b72a57d322aa4de12d11f5990556ada957d
Bogdan264 <bogdanmychkovskyi@gmail.com>
2023-03-30 11:04:38 +0300

    Renamed function and add exception

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import os
 
-filename = "text.txt"
-#keyword = input("Enter keyword: ")
-keyword = "damage"
 
+def removal_by_word_file (filename, keyword):
+    try:
+        with open(filename, "r") as f_in, open("filtered.txt", "w") as f_out:
+            # зчитуєм всі рядки з обраного файлу
+            for line in f_in:
+                # перевіряємо, чи знаходиться в обраному рядку ключове слово
+                if keyword in line:
+                    # якщо так, записуємо рядок у вихідний файл
+                    f_out.write(line)
+        print("Filtered file saved as filtered.txt")
+    except:
+        print(f"File {filename}.txt do not exist here")
 
-def delete_from_file (filename, keyword):
-    with open(filename, "r") as f_in, open("filtered.txt", "w") as f_out:
-        # читаємо кожен рядок вхідного файлу
-        for line in f_in:
-            # перевіряємо, чи містить рядок ключове слово
-            if keyword in line:
-                # якщо так, записуємо рядок у вихідний файл
-                f_out.write(line)
-
-    print("Filtered file saved as filtered.txt")
 
 if __name__ == "__main__":
     filename = "text.txt"
     keyword = "damage"
 
-    delete_from_file(filename, keyword)
+    removal_by_word_file(filename, keyword)
